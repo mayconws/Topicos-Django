94b54bd8cca7110a058b78faa7485629fb0bda70
Aluno <mayconwas2018@gmail.com>
2020-02-17 20:41:19 -0300

    Adicionando Css, Bootstrap e Js

--- Progressao/paginas/urls.py
+++ Progressao/paginas/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 # Importa as views que a gente criou 
-from .views import PaginaInicial
+from .views import PaginaInicial, PaginaSobre
 
 # Tem que ser urlpatterns porque é padrão do Django
 urlpatterns = [
     # Todo path tem endereço, sua_view.as_view() e nome
     path('', PaginaInicial.as_view(), name='index'),
+    path('sobre/', PaginaSobre.as_view(), name='sobre'),
+    
 ]
